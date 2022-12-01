f1ae211735e6d86ec0e385706bfd297072119337
Robin Kopecky <kopecky@edhouse.cz>
2022-12-02 00:17:07 +0100

    defense/attack bot

--- bot/bot.py
+++ bot/bot.py
@@ -10,7 +10,10 @@ class MarineRushBot(BotAI):
     RACE: Race = Race.Terran
     stage: int = 0
     step: int = 0
+    isAttacking: bool = False
     closeRadius: int = 8
+    groupSize: int = 0
+    attackGroup = []
 
     async def on_unit_took_damage(self, unit: UnitTypeId.MARINE, amount_damage_taken: float):
         enemyUnitAround = self.enemy_units.not_flying.closer_than(20, self.townhalls[0])
@@ -36,58 +39,69 @@ class MarineRushBot(BotAI):
 
             # Trénování SCV
             # Bot trénuje nová SCV, jestliže je jich méně než 21
-            if self.can_afford(UnitTypeId.SCV) and self.supply_workers <= 22 and command_center.is_idle:
+            if self.can_afford(UnitTypeId.SCV) and self.supply_workers <= 30 and command_center.is_idle:
                 command_center.train(UnitTypeId.SCV)
 
             # Postav Supply Depot, jestliže zbývá méně než 6 supply a je využito více než 13
-            if self.supply_left < 10 and self.supply_used >= 14 and not self.already_pending(UnitTypeId.SUPPLYDEPOT):
+            if self.supply_left < 20 and self.supply_used >= 14 and not self.already_pending(UnitTypeId.SUPPLYDEPOT):
                 if self.can_afford(UnitTypeId.SUPPLYDEPOT):
                     # Budova bude postavena poblíž Command Center směrem ke středu mapy
                     # SCV pro stavbu bude vybráno automaticky viz dokumentace
                     await self.build(
                         UnitTypeId.SUPPLYDEPOT,
-                        near=command_center.position.towards(self.game_info.map_center, 12))
+                        near=command_center)
+                if self.can_afford(UnitTypeId.SUPPLYDEPOT):
+                    # Budova bude postavena poblíž Command Center směrem ke středu mapy
+                    # SCV pro stavbu bude vybráno automaticky viz dokumentace
+                    await self.build(
+                        UnitTypeId.SUPPLYDEPOT,
+                        near=command_center)
 
             # Stavba Barracks
             # Bot staví tak dlouho, dokud si může dovolit stavět Barracks a jejich počet je menší než 6
             if self.tech_requirement_progress(UnitTypeId.BARRACKS) == 1:
                 # Je jich méně než 6 nebo se již nějaké nestaví
-                if self.structures(UnitTypeId.BARRACKS).amount < 6:
-                    if self.can_afford(UnitTypeId.BARRACKS) and not self.already_pending(UnitTypeId.BARRACKS):
+                if self.structures(UnitTypeId.BARRACKS).amount < 8:
+                    if self.can_afford(UnitTypeId.BARRACKS):
+                        print('[INFO] gonna build barracks')
                         await self.build(
                             UnitTypeId.BARRACKS,
                             near=command_center.position.towards(self.game_info.map_center, 8))
 
-            if (self.structures(UnitTypeId.BARRACKS) or self.already_pending(UnitTypeId.BARRACKS)) and self.structures(UnitTypeId.REFINERY).amount <= 4:
+            if self.structures(UnitTypeId.BARRACKS).amount > 0 and self.structures(UnitTypeId.REFINERY).amount < 2:
                 if self.can_afford(UnitTypeId.REFINERY) and not self.already_pending(UnitTypeId.REFINERY):
-                    print('gonna build refinery')
                     gs = self.vespene_geyser.closer_than(self.closeRadius, command_center)
                     for g in gs:
                         if await self.can_place(UnitTypeId.REFINERY, g.position):
                             if self.workers.gathering.exists:
+                                print('[INFO] gonna build refinery')
+                                print(self.structures(UnitTypeId.REFINERY).amount)
                                 worker = self.workers.gathering.closest_to(g)
                                 worker.build(UnitTypeId.REFINERY, g)      
 
-            if self.structures(UnitTypeId.FACTORY).amount < 1:
+            if self.structures(UnitTypeId.BARRACKS).amount > 5 and self.structures(UnitTypeId.FACTORY).amount < 1:
                 if self.can_afford(UnitTypeId.FACTORY) and not self.already_pending(UnitTypeId.FACTORY):
-                    print('gonna build factory')
+                    print('[INFO] gonna build factory')
                     await self.build(UnitTypeId.FACTORY, near=command_center.position.towards(self.game_info.map_center,8))
             
-            if self.structures(UnitTypeId.STARPORT).amount < 1:
+            if self.structures(UnitTypeId.BARRACKS).amount > 5 and self.structures(UnitTypeId.STARPORT).amount < 1:
                 if self.can_afford(UnitTypeId.STARPORT) and not self.already_pending(UnitTypeId.STARPORT):
-                    print('gonna build starport')
+                    print('[INFO] gonna build starport')
                     await self.build(UnitTypeId.STARPORT, near=command_center.position.towards(self.game_info.map_center,8))
 
             # -----------------------------------------------------------------------------------
             # Trenovani jednotek
             # -----------------------------------------------------------------------------------
 
-            # Trénování jednotky Marine
-            # Pouze, má-li bot postavené Barracks a může si jednotku dovolit
             if self.structures(UnitTypeId.BARRACKS) and self.can_afford(UnitTypeId.MARINE):
                 # Každá budova Barracks trénuje v jeden čas pouze jednu jednotku (úspora zdrojů)
                 for barrack in self.structures(UnitTypeId.BARRACKS).idle:
                     barrack.train(UnitTypeId.MARINE)
+
+            if self.structures(UnitTypeId.STARPORT).amount == 1:
+                for stp in self.structures(UnitTypeId.STARPORT).idle:
+                    if self.can_afford(UnitTypeId.VIKINGFIGHTER):
+                        stp.train(UnitTypeId.VIKINGFIGHTER)
 
             # Trénování jednotky Reaper
             # Pouze, má-li bot postavené Barracks a může si jednotku dovolit
@@ -96,54 +110,65 @@ class MarineRushBot(BotAI):
                 for barrack in self.structures(UnitTypeId.BARRACKS).idle:
                     barrack.train(UnitTypeId.REAPER)
 
-            if self.units(UnitTypeId.STARPORT).exists and self.structures(UnitTypeId.STARPORT) and not self.already_pending(UnitTypeId.STARPORT) and self.can_afford(UnitTypeId.VIKING):
-                for sp in self.structures(UnitTypeId.STARPORT):
-                    print('gonna build viking')
-                    sp.train(UnitTypeId.VIKING)
-
-            # -----------------------------------------------------------------------------------
-            # Útok s jednotkou Marine
-            # -----------------------------------------------------------------------------------
-
-            if self.enemy_units.not_flying.closer_than(15, self.townhalls[0]):
-                enemyUnitAround = self.enemy_units.not_flying.closer_than(15, self.townhalls[0])
-                for unit in self.units.filter(lambda u: u.type_id != UnitTypeId.SCV).closer_than(30, command_center):
-                    unit.attack(enemyUnitAround[0])
+            if self.enemy_units.not_flying.closer_than(40, self.townhalls[0]):
+                enemyUnitAround = self.enemy_units.not_flying.closer_than(40, self.townhalls[0])
+                for unit in self.units.filter(lambda u: u.type_id != UnitTypeId.SCV).closer_than(40, command_center):
+                    if unit not in self.attackGroup:
+                        unit.attack(enemyUnitAround[0])
             else:
-                for unit in self.units.filter(lambda u: u.type_id != UnitTypeId.SCV).closer_than(30, command_center):
+                for unit in self.units.filter(lambda u: u.type_id != UnitTypeId.SCV).closer_than(40, command_center):
+                    if unit not in self.attackGroup:
+                        unit.move(command_center)
+
+            idle_marines = self.units(UnitTypeId.MARINE)
+            idle_reapers = self.units(UnitTypeId.REAPER)
+            idle_vikings = self.units(UnitTypeId.VIKING)
+
+            for attacker in self.attackGroup:
+                if attacker not in self.units:
+                    self.attackGroup.remove(attacker)
+
+            if idle_marines.amount > 20 + self.groupSize and self.isAttacking == False:
+                print('[INFO] bot is attacking')
+                for unit in self.attackGroup:
                     unit.move(command_center)
+                
+                for i in range(int(idle_marines.amount/3)):
+                    self.attackGroup.append(idle_marines[i])
+                
+                for i in range(int(idle_reapers.amount/3)):
+                    self.attackGroup.append(idle_reapers[i])
 
-            idle_marines = self.units(UnitTypeId.MARINE).idle
-            idle_reapers = self.units(UnitTypeId.REAPER).idle
-            idle_vikings = self.units(UnitTypeId.VIKING).idle
+                for i in range(int(idle_vikings.amount/3)):
+                    self.attackGroup.append(idle_vikings[i])
 
-            if idle_marines.amount > 50 or idle_reapers.amount > 30 and idle_vikings.amount > 5:
+                self.isAttacking = True
+
                 target = self.enemy_structures.random_or(
                     self.enemy_start_locations[0]).position
-                for marine in idle_marines:
-                    marine.attack(target)
-                for reaper in idle_reapers:
-                    reaper.attack(target)
-                for viking in idle_vikings:
-                    viking.attack(target)
+
+                for unit in self.attackGroup:
+                    unit.attack(target)
+
+            if self.isAttacking and len(self.attackGroup) == 0:
+                print('[INFO] Attack group dismiss')
+                self.attackGroup = []
+                self.isAttacking = False
 
             # Zbylý SCV bot pošle těžit minerály nejblíže Command Center
-            rafineryCount = self.structures(UnitTypeId.REFINERY).amount
-            count = rafineryCount*3
-            i = 0
             for scv in self.workers.idle:
-                if i < count and rafineryCount > 0:
-                    print('going to work to refinery')
-                    scv.gather(self.structures(UnitTypeId.REFINERY).closest_to(command_center))
-                else:
-                    scv.gather(self.mineral_field.closest_to(command_center))
-                i += 1
+                scv.gather(self.mineral_field.closest_to(command_center))
 
-            if self.step == 180:
+            if self.step == 360:
                 self.closeRadius = 30 
-            
+
             if self.step % 30 == 0:
+                self.groupSize += 1
+                await self.distribute_workers()
                 print('------------------------')
                 print('marine',self.units.filter(lambda u: u.type_id == UnitTypeId.MARINE).amount)
                 print('reapers',self.units.filter(lambda u: u.type_id == UnitTypeId.REAPER).amount)
-                print('vikings',self.units.filter(lambda u: u.type_id == UnitTypeId.VIKING).amount)
+                print('vikings',self.units.filter(lambda u: u.type_id == UnitTypeId.VIKINGFIGHTER).amount)
+                print('[INFO] ' + str(len(self.attackGroup)))
+                print('[INFO] ' + str(self.isAttacking))
+                print('------------------------')
